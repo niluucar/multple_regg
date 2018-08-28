5f1c06a897c7d615128608a00e0476cf44a0602d
Nilüfer <ucarnilufer@gmail.com>
2018-08-28 17:04:01 +0200

    last_version

--- R_M_R.R
+++ R_M_R.R
@@ -12,8 +12,31 @@ attributes(ExistingProdNiluPau)
 
 str(ExistingProdNiluPau)
 
+summary(ExistingProdNiluPau)
 ####Added zeros as N/A ####
 ExistingProdNiluPau[is.na(ExistingProdNiluPau)] <- "0"
 sum(is.na(ExistingProdNiluPau)) #total count of na's in the data set 
+sum(is.na(ExistingProdNiluPau$Best_seller_rank))
+
+####Removing Outliers####
+
+ggplot(data=ExistingProdNiluPau, aes(x=ExistingProdNiluPau$Volume)) + 
+  geom_histogram(bindwith=0.5, col="black",fill="lightgreen", alpha = .7) #+xlim(c(0, 1500))
+#Acording to this graph, we can see outliers obviously. 
+#We should remove them for getting best result before running the model.
+
+ExistingProdNiluPau = ExistingProdNiluPau[ExistingProdNiluPau$Volume <=6000,]
+
+####Product_num attributes
+#We should not use this variable into the model. This attributes are only Id numbers of products.
+#So,they will not make sense to use in the model.We should replace all romws of it null.
+
+ExistingProdNiluPau$Product_ID<-NULL
+
+
+####Graphs ####
+
+ggplot(data = ExistingProdNiluPau,mapping = aes(x = Best_seller_rank,fill=Volume))+geom_histogram()+
+  geom_text(stat="count",aes(label=..count..,y=..count..), vjust=10)
 
 
