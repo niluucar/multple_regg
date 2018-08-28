8226c94186e0750cc5b51ec28cea990736b36b5d
Nilüfer <ucarnilufer@gmail.com>
2018-08-28 10:05:02 +0200

    First_upload_of_database

---
+++ R_M_R.R
@@ -0,0 +1,6 @@
+setwd("C:/Users/Lenovo/Desktop/Ubiqum_data/task_6/github_3")
+
+library(readr)
+newproductNiluPau <- read_delim("newproductNiluPau.csv", 
+                                ";", escape_double = FALSE, trim_ws = TRUE)
+View(newproductNiluPau)
