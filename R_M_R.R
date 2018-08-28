db233901dd2435f5eba3c52bbf2bd7a100c28ba1
Nilüfer <ucarnilufer@gmail.com>
2018-08-28 10:35:03 +0200

    added_my_path

--- R_M_R.R
+++ R_M_R.R
@@ -1,5 +1,5 @@
 setwd("C:/Users/pauve/Documents/UBIQUM/SCANS/PRACTICA6-NILUPAU/RSTUDIO")
-
+setwd("C:/Users/Lenovo/Desktop/Ubiqum_data/task_6/github_3")
 library(readr)
 newproductNiluPau <- read_delim("NiluPau.csv", 
                                 ";", escape_double = FALSE, trim_ws = TRUE)
