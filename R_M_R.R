aad75030bf222ac84da4c039f5b29cd735025a74
Pau Vela <pau.vela.garcia@gmail.com>
2018-08-28 11:25:31 +0200

    EXISTINGPRODUCTNAME

--- R_M_R.R
+++ R_M_R.R
@@ -1,10 +1,10 @@
 setwd("C:/Users/pauve/Documents/UBIQUM/SCANS/PRACTICA6-NILUPAU/RSTUDIO")
 setwd("C:/Users/Lenovo/Desktop/Ubiqum_data/task_6/github_3")
 library(readr)
-newproductNiluPau <- read_delim("NiluPau.csv", 
+ExistingProdNiluPau <- read_delim("NiluPau.csv", 
                                 ";", escape_double = FALSE, trim_ws = TRUE)
-View(newproductNiluPau)
+View(ExistingProdNiluPau)
 
 ####MISSING VALUES####
 
-attributes(newproductNiluPau)
+attributes(ExistingProdNiluPau)
